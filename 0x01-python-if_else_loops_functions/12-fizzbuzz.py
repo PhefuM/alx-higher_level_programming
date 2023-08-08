e1fbfa0c91322fd22a20f4d59b3eeada4c9258dd
PhefuM <phefumadyo@gmail.com>
2023-08-08 16:38:14 -0700

    prints the numbers from 1 to 100 separated by a space

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -6,12 +6,11 @@ def fizzbuzz():
     For multiples of three and five, print FizzBuzz instead of the number.
     """
     for number in rabge(1, 101):
-        if number % 3 == 9 and number % 5 == 0:
+        if number % 3 == 0 and number % 5 == 0:
             print("FizzBuzz ", end="")
         elif number % 3 == 0:
             print("Fizz ", end="")
         elif number % 5 == 0:
             print("Buzz ", end="")
         else:
-            print("{} ".format(number), end=""
-
+            print("{} ".format(number), end="")
