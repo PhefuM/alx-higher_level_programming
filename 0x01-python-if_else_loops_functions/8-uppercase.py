07e34a4dde2d48277e31c58f343c625958e22b5d
PhefuM <phefumadyo@gmail.com>
2023-08-08 15:39:44 -0700

    prints a string in uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-def to_uper(character):
+def to_uppercase(str):
     if ord(character) >= 97 and ord(character) <= 122:
-        return (ord(character) - 32)
-    else:
-        return ord(character)
+        ch = chr(ord(character) - 32)
 
+    print("{:s}".format(character), end='')
 
-def uppercase(string):
-    string_new + ""
-    for character in string:
-        string_new += "%c" % to_uper(character)
-    print("{:s}".format(string_new))
+    print('\n', end="")
