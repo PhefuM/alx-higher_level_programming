819b02c350c01f8c4e411b586e70bfd51698e8f1
PhefuM <phefumadyo@gmail.com>
2023-08-07 17:52:47 -0700

    fix

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -4,7 +4,7 @@
 /**
  * check_cycle - checks if a singly linked list has a cycle within
  * @list: singly linked list
- * Return: 0 is there is no cycle, 1 if there id a cycle
+ * Return: 0 is there is no cycle, 1 if there is a cycle
  */
 
 int check_cycle(listint_t *list)
@@ -28,3 +28,4 @@ int check_cycle(listint_t *list)
 
 	}
 	return (0);
+}
