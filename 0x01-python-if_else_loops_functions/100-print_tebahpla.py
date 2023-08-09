756f4f99645a58fdf8e990062a563310b3cbca5f
PhefuM <phefumadyo@gmail.com>
2023-08-08 17:48:49 -0700

    Prints alphabet in reverse

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(122, 96, -1):
-    if i % 2 == 0
+    if i % 2 == 0:
         print('{:c}'.format(122 - i), end='')
     else:
         print('{:c}'.format(90 - i), end='')
