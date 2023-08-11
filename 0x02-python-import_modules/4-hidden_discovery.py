b0700515d7bd0e66a6bdbbba57551894099f5d1d
PhefuM <phefumadyo@gmail.com>
2023-08-11 08:36:33 -0700

    prints all the names defined by the compiled module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,19 +1,6 @@
 #!/usr/bin/python3
-import importlib.util
-
-if __name__ == "__main__":
-    file_path = "hidden_4pyc"
-    spec = importlib.util.spec_from_file_location("hidden_4p", file_path)
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-    #Extract the names that do not start with "__"
-    names = sorted(
-                names = sorted(
-                    name
-                    for name in dir(module)
-                    if not name.startswith("__")
-                )
-                #Print the name
-                
-                for name in names:
-                    print(name)
+if __name == "__main__":
+    import hidden_4
+    for name in dir(hidden_4):
+        if name[0] != '-' and name[1] != '_':
+            print(name)
