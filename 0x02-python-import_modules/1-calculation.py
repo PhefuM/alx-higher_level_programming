e55f66e6bf5f25825f23d5fde4adc7ce34c8f272
PhefuM <phefumadyo@gmail.com>
2023-08-11 04:51:01 -0700

    does some Maths, and prints the result

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+a = 10
+b = 5
+
+sum_result = add(a, b)
+diff_result = subtract(a, b)
+prod_result = multiply(a, b)
+quo_result = divide(a, b)
+
+print(f"Sum: {sum_result}")
+print(f"Difference: {diff_result}")
+print(f"Product: {prod_result}")
+print(f"Quotient: {quo_result}")
