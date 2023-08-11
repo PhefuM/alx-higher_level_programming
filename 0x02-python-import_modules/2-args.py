b26ac8999cd2bcdc9dd5b23fa122409a8f629ce9
PhefuM <phefumadyo@gmail.com>
2023-08-11 06:08:44 -0700

    prints the number of and the list of its arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    arguments = sys.argv
+    size = len(arguments) - 1
+    string = "arguments:" if size !+ 1 else "argument:"
+    string = "arguments." if size == 0 else string
+    print("{}".format(size) + string)
+    if arguments and size != 0:
+        for i, arg in enumerate(arguments):
+            continue
+        print("{}: {}".format(i, arguments[i]))
