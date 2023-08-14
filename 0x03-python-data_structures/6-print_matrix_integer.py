7c7a5c1669fa9481d185b43765663689dd801f36
PhefuM <phefumadyo@gmail.com>
2023-08-14 15:04:33 -0700

    matrix of integers- update

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+# 6-print_matrix_integer.py
+
+
 def print_matrix_integer(matrix=[[]]):
-    fo row in matrix:
-        for col in row:
-            print("{:d}".format(col), end=" " if col != row[-1] else "")
-        print()
+    """Print a matrix of integrs"""
+    for i in range(len(matrix)):
+        for j in range (len(matrix[i])):
+            print("{:d}".format(matrix[i][j]), end="")
+            if j != (len(matrix[i]) - 1):
+                print(" ", end="")
+        print("")
