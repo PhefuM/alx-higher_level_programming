5b0ec91a04274d1b9eb21b4b04fa63238810972d
PhefuM <phefumadyo@gmail.com>
2023-08-14 17:53:09 -0700

    fix

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,4 +1,4 @@
-#!/us/bin/python3
+#!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
@@ -9,4 +9,3 @@ def new_in_list(my_list, idx, element):
         my_list2[idx] = element
         return my_list2
     return my_list
-
