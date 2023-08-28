8c4f9b553a4f5cd24b6662a23c45a6315c986626
PhefuM <phefumadyo@gmail.com>
2023-08-28 16:02:07 -0700

    updates

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 
 def safe_print_list(my_list=[], x=0):
     num_elements = 0
-    for num_elements in range(x):
+    for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
             num_elements += 1
