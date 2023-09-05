04939b600570f73bd2376a6bbe0e6dbccf1487db
PhefuM <phefumadyo@gmail.com>
2023-09-04 20:02:05 -0700

    Instance deletion

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -7,7 +7,7 @@ Defines a class Rectangle
 class Rectangle:
     """Representation of a rectangle"""
 
-    number of instances = 0
+    number_of_instances = 0
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
