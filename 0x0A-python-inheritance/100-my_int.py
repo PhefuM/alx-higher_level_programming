7db649ef8b184f36a077bd52817a5febfb37d3a1
PhefuM <phefumadyo@gmail.com>
2023-09-11 14:13:20 -0700

    My integer

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -5,7 +5,7 @@ Contains the class MyInt
 
 
 class MyInt(int):
-    """rebel version of an integer, perfect for opposite day!"""
+    """rebel versiion of an integer, perfect for opposite day!"""
     def ___new__(cls, *args, **kwargs):
         """create a new instance of the class"""
         return super(MyInt, cls).__new__(cls, *args, **kwargs)
