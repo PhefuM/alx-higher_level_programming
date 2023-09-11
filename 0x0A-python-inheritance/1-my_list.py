9a267bd8605caf8c4c932ecd104fa03511ea22ae
PhefuM <phefumadyo@gmail.com>
2023-09-11 14:24:24 -0700

    testin

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""
-contains the MyList class
+"""This contains a class definition which extends the list class
+adding an instance method to produce a sorted replica of the list
 """
 
 
 class MyList(list):
-    """a subclass of list"""
-    def __init__(self):
-        """initializes the object"""
-        superr().__init__()
+    """This class extends the list class
+
+    it has an added instance method which prints the list in  sorted ascending
+    format.
+    """
 
     def print_sorted(self):
-        """prints the sorted list"""
+        """prints a sorted ascending format of the list."""
         print(sorted(self))
