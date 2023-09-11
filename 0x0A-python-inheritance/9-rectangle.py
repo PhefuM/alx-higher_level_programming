a5e17b9adf4f95edc525d9b2c9eb52c2fdc46f8d
PhefuM <phefumadyo@gmail.com>
2023-09-11 13:38:19 -0700

    rectangle - full

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -20,17 +20,17 @@ class BaseGeometry:
 
 class Rectangle(BaseGeometry):
     """A representaion of a rectangle"""
-     def __init__(self, width, height):
-         """instantiation of the rectangle"""
-         self.integer_validator("width", width)
-         self.__width = width
-         self.integer_validator("height", height)
-         self.__height = height
+    def __init__(self, width, height):
+        """instantiation of the rectangle"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
 
-     def area(self):
-         """returns the area of the rectangle"""
-         return self.__width * self.__height
+    def area(self):
+        """returns the area of the rectangle"""
+        return self.__width * self.__height
 
-     def __str__(self):
-         """informal string representation of the rectangle"""
-         return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+    def __str__(self):
+        """informal string representation of the rectangle"""
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
