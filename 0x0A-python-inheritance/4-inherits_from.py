02bffd85af1c8cbe27e2aa23dbe905f99e031dd2
PhefuM <phefumadyo@gmail.com>
2023-09-11 10:03:25 -0700

    only sub class of

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,5 +5,5 @@ Contains the is_kind_of_class function
 
 
 def inherits_from(obj, a_class):
-    """True if obj is an instance or inherited from a_class, else False"""
+    """returns true if obj is a subclass of  a_class, otherwise false"""
     return (isinstance(type(obj), a_class) and type(obj) != a_class)
