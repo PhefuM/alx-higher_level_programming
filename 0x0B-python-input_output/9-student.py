d92317833c66ff820fc1a81d7711b4039a5bd790
PhefuM <phefumadyo@gmail.com>
2023-09-12 14:49:12 -0700

    a class Student that defines a student

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -5,12 +5,14 @@
 class Student:
     """Represents a student"""
 
+
 def __init__(self, first_name, last_name, age):
     """Initializes a new Student
     """
     self.first_name = first name
     self.last_name = last name
     sef.age = age
+
 
 def to_json(self):
     """Retrieves dictionary representaion of a Student"""
