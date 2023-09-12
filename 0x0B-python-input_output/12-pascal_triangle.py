3427eb69fc8f9c2750eda77b8a999f43d7f93def
PhefuM <phefumadyo@gmail.com>
2023-09-12 16:06:13 -0700

    Pascal’s triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -9,7 +9,7 @@ def pascal_triangle(n):
         return[]
 
     triangles = [[1]]
-    while len(triagles) != n:
+    while len(triangles) != n:
         tri = triangles[-1]
         tmp = [1]
         for i in range(len(tri) - 1):
