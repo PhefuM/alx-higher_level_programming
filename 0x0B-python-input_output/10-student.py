68264c09eda35776e1df6be213b888b8bad9e413
PhefuM <phefumadyo@gmail.com>
2023-09-12 15:06:56 -0700

    Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module defines a Student"""
+
+
+class Student:
+    """Represents a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student
+        If attrs is a list of strings, only attribute names contained in this list must be retrieved
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) ofr k in attrs if hasattr(self, k)}
+        return slef.__dict__
