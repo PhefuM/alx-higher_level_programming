aa9628ef251b613ed3c1b510f0737ddc88a6fdeb
PhefuM <phefumadyo@gmail.com>
2023-09-12 10:12:42 -0700

    writes a string to a text file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,5 +5,5 @@
 def write_file(filename="", text=""):
     """Writes a string to a UTF8 text file
     """
-    with open(filenme, "w", encoding="utf-8") as f:
+    with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
